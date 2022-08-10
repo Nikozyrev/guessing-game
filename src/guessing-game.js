e832c113e09b92c43a447cbb880f9f23146c0e3b
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2022-08-10 17:36:15 +0300

    feat: first version - 89 passing

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,27 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.range = Array.from({ length: max - min + 1 }, (n, i) => i + min)
     }
 
     guess() {
-
+        this.result = this.range[Math.round(this.range.length / 2)]
+        return this.result
     }
 
     lower() {
-
+        this.range = this.range.slice(0, this.range.indexOf(this.result) )
     }
 
     greater() {
-
+        this.range = this.range.slice(this.range.indexOf(this.result) )
     }
 }
+
+const game = new GuessingGame
+game.setRange(0, 20)
+game.guess()
+game.lower()
+console.log(game);
 
 module.exports = GuessingGame;
